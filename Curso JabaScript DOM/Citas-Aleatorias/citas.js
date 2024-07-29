3d8d2cd84ac56af04224cb7090e03f87b4635ff4
GregorioRincones <gregorio.rinconesb01@gmail.com>
2024-07-29 07:26:31 -0400

    ingreso lista de tareas

--- Curso JabaScript DOM/Citas-Aleatorias/citas.js
+++ Curso JabaScript DOM/Citas-Aleatorias/citas.js
@@ -1,50 +1,50 @@
 let citas = [
     {
-        'autor': 'Albert  0',
+        'autor': 'Albert',
         'texto': 'No poseo talentos especiales, solo soy apacionadamente curioso',
     },
     {   
-        'autor': 'Anonimo 1',
+        'autor': 'Anonimo',
         'texto': 'Los carros son para rodarlos y los aviones para volarlos',
     },
     {   
-        'autor': 'Anonimo 2',
+        'autor': 'Anonimo',
         'texto': 'Perro que come manteca,, mete la langua en tapara',
     },
     {   
-        'autor': 'Anonimo 3',
+        'autor': 'Anonimo',
         'texto': 'La mujer que quiere a dos, tambien puede querer a tres y hasta cuatro',
     },
     {   
-        'autor': 'Anonimo 4 ',
+        'autor': 'Anonimo',
         'texto': 'El indio soba la india sin motivo y sin razon',
     },
     {   
-        'autor': 'Alan Kay 5',
+        'autor': 'Alan Kay',
         'texto': 'La mejor forma de predecir el futuro es inventandolo',
     },
     {   
-        'autor': 'Amelia Heathat 6',
+        'autor': 'Amelia Heathat',
         'texto': 'Lo mas dificil es tomar la decision de actuar, el resto es muy facil',
     },
     {   
-        'autor': 'Anonimo 7',
+        'autor': 'Anonimo',
         'texto': 'pepe pina pica papa, papa pica pepe pina',
     },
     {   
-        'autor': 'Anonimo 8',
+        'autor': 'Anonimo',
         'texto': 'un burro en el decierto lo que hace es sombra',
     },
     {   
-        'autor': 'Anonimo 9',
+        'autor': 'Anonimo',
         'texto': 'a caballo regalado no se le miran los dientes',
     },
     {   
-        'autor': 'Anonimo 10',
+        'autor': 'Anonimo',
         'texto': 'El Zorro cambia de pelo , pero no cambia sus manas ',
     },
     {   
-        'autor': 'Mark Twain 11',
+        'autor': 'Mark Twain',
         'texto': 'Los dos dias mas importantes de tu vida son, el que nacistes y el dia que descubres para que',
     },
 ];
